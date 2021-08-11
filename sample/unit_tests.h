4dc5e7b0391ab7bcba0a09bbd3ca9805ba86665b
matt russell <mrussell@cs.tufts.edu>
2021-08-10 21:34:05 -0400

    readme

--- sample/unit_tests.h
+++ sample/unit_tests.h
@@ -6,7 +6,16 @@ void test_pass() {
     assert(0 == 0);
 }
 
+void test_pass_0() {
+    assert(1 == 1);
+}
+
 // TESTSET FAIL
 void test_fail() {
     assert(0 == 1);
+}
+
+// VALGRIND FAIL
+void valgrind_fail() {
+    int* x = new int[100];
 }
